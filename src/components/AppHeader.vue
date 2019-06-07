09f6a2db142d84044a1e76e199d943f1c41e3eab
MarkoPop <marko.v.popovic@gmail.com>
2019-06-07 12:32:15 +0200

    image app

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <router-link to="/" class="active item">Home</router-link>
+        <div class="right menu">
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item">Gallery</router-link>
+                <router-link to="/upload" class="item">Upload</router-link>
+                <a class="item" @click="logout">Logout</a>
+            </div>
+            <a v-else class="ui item" @click="login">
+                Log In
+            </a>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { mapActions, mapGetters } from 'vuex';
+
+    export default {
+        name:'AppHeader',
+        computed: mapGetters(['isLoggedIn']),
+        methods: {
+            ...mapActions(['login','logout'])
+        }
+    }
+</script>
+
+<style scoped>
+    .horizontal{
+        display: flex;
+        flex-direction: row;
+    }
+</style>
